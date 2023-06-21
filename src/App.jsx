7a34aa7353745a6869b47d92e580f2f561381ebe
Rente6 <84778080+Rente6@users.noreply.github.com>
2023-06-21 16:24:43 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Assinar from "./Assi";
+
+function App(){
+  return(
+    <div className="fundo">
+      <Assinar/>
+    </div>
+  )
+}
+export default App
---
+++ src/Assi.jsx
@@ -0,0 +1,17 @@
+import "../src/Assi.css"
+
+function Assinar(){
+    return(
+        <>
+        <div className="oi">
+            <h1>Plano</h1>
+            <p>Assine nosso plano e tenha 
+seu plano alimentar e o controle de sua atividades.
+Plano de custos a partir de R$100,00.</p>
+
+    <button className="entrar">Assinar</button>
+        </div>
+        </>
+    )
+}
+export default Assinar
