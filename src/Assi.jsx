2ef84bbdd84f6cadb0ee87ec5b455c89856c8a26
Rente6 <84778080+Rente6@users.noreply.github.com>
2023-06-26 09:40:53 -0300

    Add files via upload

--- src/Assi.jsx
+++ src/Assi.jsx
@@ -1,8 +1,11 @@
 import "../src/Assi.css"
-
+import Ndecoracao from "../src/assets/Ndecoracao.png"
+import Bola1 from "../src/assets/Bola1.png"
+import Bola2 from "../src/assets/Bola2.png"
 function Assinar(){
     return(
         <>
+<img src={Bola1} alt=""  className="bola1"/>
         <div className="oi">
             <h1>Plano</h1>
             <p>Assine nosso plano e tenha 
@@ -11,6 +14,10 @@ Plano de custos a partir de R$100,00.</p>
 
     <button className="entrar">Assinar</button>
         </div>
+        <div className="enfeite">
+         <img src={Ndecoracao} alt="" />
+         </div>
+         <img src={Bola2} alt=""  className="bola2"/>
         </>
     )
 }
